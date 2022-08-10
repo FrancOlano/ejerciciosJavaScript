5bb5a095eefad6b90281ecf3bcd96d824bbda265
FrancOlano <olanofranco1@gmail.com>
2022-08-10 13:55:50 +0200

    ej14 para terminar

---
+++ ej14/main.js
@@ -0,0 +1,23 @@
+// 1. A partir de la pizza sustituías
+// todos los elemento del array por
+// cervezas:🍔🌯🍣🍕🍜🍱🍙🍘🥩  🍺
+
+let arrayUno =['🍔', '🌯', '🍣', '🍕', '🍜', '🍱', '🍙', '🍘', '🥩']
+
+
+// 2. Encontrar si existe un elemento en el array que sea una piña.
+// 🍕🍕🍍🍕🍕
+
+// 3. Quita la piña del siguiente array.
+// 🍕🍕🍍🍕🍕
+
+// 4. A partir del siguiente array 🍓🍋🍓🍋🍓
+// convierte todas las fresas en 🍄
+
+// 5. Añade el siguiente icono 🥵 inmediatamente después del cada
+// 🌶️ en el siguiente array:
+// 🌶️🥛🌶️🥛🌶️🥛
+
+// 6. Añade una 🃏 cartas comodín entre medio de dos cartas . Por
+// ejemplo:🎴🃏🎴 En el siguiente array:
+// 🎴🎴🎴🃏🎴🎴🎴
