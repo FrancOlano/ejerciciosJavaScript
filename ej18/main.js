95f3051f9a400f0de6f32a9f965d7d09649e1303
FrancOlano <olanofranco1@gmail.com>
2022-08-12 13:02:29 +0200

    ej18

---
+++ ej18/main.js
@@ -0,0 +1,14 @@
+$('#1').hover(function () {
+        $('#1').attr('src', './card-front.png');
+        
+    }, function () {
+        $('#1').attr('src', './card-back.png');
+    }
+);
+$('#2').hover(function () {
+    $('#2').attr('src', './card-front.png');
+    
+}, function () {
+    $('#2').attr('src', './card-back.png');
+}
+);
